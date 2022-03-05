d1a41de2424a158571e6fc8a9e09329ce425e3d9
Knziee <16rhuan16@gmail.com>
2022-03-05 18:01:25 -0300

    feat: funcao atualizar,remover,listar, procurar regioes adicionadas porem não funcionais ainda

--- api-cafe/src/controllers/controllerRegiao.js
+++ api-cafe/src/controllers/controllerRegiao.js
@@ -1,4 +1,5 @@
 const {Regiao} = require("../model")
+const Sequelize = require('sequelize');
 
 const controllerRegiao = {
     async cadastrarRegiao(req, res) {
@@ -18,6 +19,79 @@ const controllerRegiao = {
             
         }
 
+    },
+
+    async listarRegioes(req,res)  {
+            try {
+            const listaDeReigoes = await Regiao.findAll();
+            res.status(201).json(listaDeReigoes)
+            console.log(listaDeReigoes)
+        } catch(err) {
+            res.status(401).json("Erro ao tentar encontrar regiões")
+        }
+    },
+
+    async deletarRegiao(req,res)  {
+        try {
+        const { id } = req.params //parametro que a gente tem que ver como vai add;
+        console.log("Produto deletado!")
+        await Regiao.destroy({
+            where: {
+                id
+            }});
+        res.status(204).json("Região deletada!")
+    } catch(err) {
+        res.status(401).json("Erro ao tentar deletar região!")
     }
+},
+
+    async atualizarRegiao(req,res) {
+        try {
+            console.log("Regiao atualizada!")
+            const { id } = req.params //parametro que a gente tem que ver como vai add;
+            const { foto_bandeira, pais, descricao, colocacao } = req.body;
+            if(!id) return res.status(400).json("id não enviado")
+
+            const regiaoAtualizada = await Regiao.update ({
+                foto_bandeira,
+                pais,
+                descricao,
+                colocacao
+            },
+            {
+                where: {
+                    id,
+                }
+            });
+            res.json ("Região atualizada!")
+            console.log("Regiao atualizada!")
+        } catch(err) {
+            res.status(401).json("Erro ao tentar atualizar região!")
+        }
+    },
+
+
+        async pesquisaRegioes(req, res) {
+            try {
+            const { valorDaPesquisa } = req.query 
+
+            Regiao.findAll({ 
+                where: { 
+                    pais: {
+                        [Op.like]: '%' + valorDaPesquisa + '%' 
+                    } } } 
+
+                    || // alguem que manja me diz se isso é aplicavel aquikkk
+
+                    { where: {
+                         descricao: {
+                             [Op.like]: '%' + valorDaPesquisa + '%' 
+                            } } })
+        } catch(err) {
+            res.status(401).json("Nada encontrado")
+        }
+    },
+
 }
+
 module.exports = controllerRegiao
--- api-cafe/src/routers/index.js
+++ api-cafe/src/routers/index.js
@@ -3,5 +3,9 @@ const controllerRegiao = require('../controllers/controllerRegiao');
 const route = express.Router();
 
 route.post("/cadastrar-regiao", controllerRegiao.cadastrarRegiao)
+route.get("/regioes", controllerRegiao.listarRegioes)
+route.delete("", controllerRegiao.deletarRegiao)
+route.put("", controllerRegiao.atualizarRegiao)
+route.get("/search", controllerRegiao.pesquisaRegioes)
 
 module.exports =route

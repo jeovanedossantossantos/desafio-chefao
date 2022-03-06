334d9b4daa5b0513b2ae80d0f102fdcc8f229b18
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-03-06 12:10:20 -0300

    Mesclagem de branches Rhuan para main e ajustes da rota search

--- api-cafe/src/controllers/controllerRegiao.js
+++ api-cafe/src/controllers/controllerRegiao.js
@@ -1,6 +1,6 @@
-const {Regiao} = require("../model")
+const { Regiao } = require("../model")
 const Sequelize = require('sequelize');
-
+const Op = Sequelize.Op;
 const controllerRegiao = {
     async cadastrarRegiao(req, res) {
         const { foto_bandeira, pais, descricao, colocacao } = req.body
@@ -14,81 +14,118 @@ const controllerRegiao = {
                 colocacao
             })
             res.status(201).json(newRegiao)
-        }catch(err){
-            res.status(401).json({"messagem": "Ouve um problema ao cadastra uma região!!"})
-            
+        } catch (err) {
+            res.status(401).json({ "messagem": "Ouve um problema ao cadastra uma região!!" })
+
         }
 
     },
 
-    async listarRegioes(req,res)  {
-            try {
+    async listarRegioes(req, res) {
+        try {
             const listaDeReigoes = await Regiao.findAll();
             res.status(201).json(listaDeReigoes)
-            console.log(listaDeReigoes)
-        } catch(err) {
-            res.status(401).json("Erro ao tentar encontrar regiões")
+            // console.log(listaDeReigoes)
+        } catch (err) {
+            res.status(401).json({ "messagem": "Erro ao tentar encontrar regiões" })
         }
     },
 
-    async deletarRegiao(req,res)  {
+    async deletarRegiao(req, res) {
         try {
-        const { id } = req.params //parametro que a gente tem que ver como vai add;
-        console.log("Produto deletado!")
-        await Regiao.destroy({
-            where: {
-                id
-            }});
-        res.status(204).json("Região deletada!")
-    } catch(err) {
-        res.status(401).json("Erro ao tentar deletar região!")
-    }
-},
+            const { id } = req.params //parametro que a gente tem que ver como vai add;
+            // console.log("Produto deletado!")
+            await Regiao.destroy({
+                where: {
+                    id
+                }
+            });
+            res.status(201).json({ "messagem": "Região deletada!" })
+        } catch (err) {
+            res.status(401).json({ "messagem": "Erro ao tentar deletar região!" })
+        }
 
-    async atualizarRegiao(req,res) {
+    },
+
+    async atualizarRegiao(req, res) {
         try {
-            console.log("Regiao atualizada!")
+            // console.log("Regiao atualizada!")
             const { id } = req.params //parametro que a gente tem que ver como vai add;
             const { foto_bandeira, pais, descricao, colocacao } = req.body;
-            if(!id) return res.status(400).json("id não enviado")
+            if (!id) return res.status(400).json({ "messagem": "id não enviado" })
 
-            const regiaoAtualizada = await Regiao.update ({
+            const regiaoAtualizada = await Regiao.update({
                 foto_bandeira,
                 pais,
                 descricao,
                 colocacao
             },
-            {
-                where: {
-                    id,
-                }
-            });
-            res.json ("Região atualizada!")
-            console.log("Regiao atualizada!")
-        } catch(err) {
-            res.status(401).json("Erro ao tentar atualizar região!")
+                {
+                    where: {
+                        id,
+                    }
+                });
+            res.status(201).json({ "messagem": "Região atualizada!" })
+            // console.log("Regiao atualizada!")
+        } catch (err) {
+            res.status(401).json({ "messagem": "Erro ao tentar atualizar região!" })
         }
     },
 
 
-        async pesquisaRegioes(req, res) {
-            try {
-            const { valorDaPesquisa } = req.query 
+    async pesquisaRegioes(req, res) {
+        var listaPesuisa = []
+        // 
+        try {
+            const { valorDaPesquisa } = req.query
+            // const {pais, descricao} = req.query]
+            console.log(valorDaPesquisa)
 
-            Regiao.findAll({ 
-                where: { 
+            const resultadDeBuscaPais = await Regiao.findAll({
+                where: {
                     pais: {
-                        [Op.like]: '%' + valorDaPesquisa + '%' 
-                    } } } 
+                        [Op.like]: '%' + valorDaPesquisa + '%'
+                    }
+                }
+            },
 
-                    || // alguem que manja me diz se isso é aplicavel aquikkk
+                // || // alguem que manja me diz se isso é aplicavel aquikkk
+                // kkkkkkkkkk acredito que não deve funcionar do jeito que você imagina 
+            )
 
-                    { where: {
-                         descricao: {
-                             [Op.like]: '%' + valorDaPesquisa + '%' 
-                            } } })
-        } catch(err) {
-            res.status(401).json("Nada encontrado")
+            const resultadDeBuscaDes = await Regiao.findAll({
+
+                where: {
+                    descricao: {
+                        [Op.like]: '%' + valorDaPesquisa + '%'
+                    }
+                }
+
+            })
+            // console.log(json(resultadDeBuscaDes))
+            // res.status(201).json(resultadDeBuscaDes)
+
+            if (resultadDeBuscaPais.length) {
+                // res.status(201).json(resultadDeBuscaPais)
+                console.log("Pais")
+                listaPesuisa.push(resultadDeBuscaPais)
+            }
+            if (resultadDeBuscaDes.length) {
+                // res.status(201).json(resultadDeBuscaDes)
+                console.log("Desc")
+                listaPesuisa.push(resultadDeBuscaDes)
+
+            }
+            if(listaPesuisa.length !== 0){
+                res.status(201).json(listaPesuisa[0])
+            }else{
+                res.status(500).json({ "messagem": "Nada encontrado" })
+            }
+           //Aqui ele pega apenas a primeria pocisão 
+
+
+        } catch (err) {
+            res.status(401).json({ "messagem": "Ouve algum erro" })
         }
     },
 

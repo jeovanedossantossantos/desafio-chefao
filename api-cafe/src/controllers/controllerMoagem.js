f3d120d80c8653697c8447eb6b3e841f98618e86
willian <thewillian@hotmail.com>
2022-03-19 13:51:41 -0300

    Criacao das funcoes do controller moagem

--- api-cafe/src/controllers/controllerMoagem.js
+++ api-cafe/src/controllers/controllerMoagem.js
@@ -2,8 +2,27 @@ const {Moagem} = require("../model")
 const Sequelize = require('sequelize');
 
 const controllerMoagem = {
-    async cadastrarMoagem(req, res) {},
-    async listarMoagem(req, res){},
+    async cadastrarMoagem(req, res) {
+        const {nome} = req.body;
+
+        try {
+            const newMoagem = await Moagem.create({
+                nome
+            })
+            res.status(200).json(newMoagem) 
+            }catch(err) {
+                res.status(400).json(err)
+            }
+    },
+    async listarMoagem(req, res){
+     
+        try {
+            const lista_moagem = await Moagem.findAll()
+            res.status(200).json(lista_moagem)
+        }catch (err) {
+            res.status(400).json(lista_moagem)
+        }
+    },
     async buscarUmMoagem(req, res){},
     async deletarMoagem(req, res) {},
     async atualizarMoagem(req, res) {}

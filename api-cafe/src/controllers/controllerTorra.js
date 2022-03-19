e7875d4e4fd604cc40d187ac401348ebc9124f45
willian <thewillian@hotmail.com>
2022-03-19 15:00:04 -0300

    Construcao do controller torra

--- api-cafe/src/controllers/controllerTorra.js
+++ api-cafe/src/controllers/controllerTorra.js
@@ -2,10 +2,76 @@ const {Torra} = require("../model")
 const Sequelize = require('sequelize');
 
 const controllerTorra = {
-    async cadastrarTorra(req, res) {},
-    async listarTorra(req, res){},
-    async buscarUmTorra(req, res){},
-    async deletarTorra(req, res) {},
-    async atualizarTorra(req, res) {}
+    async cadastrarTorra(req, res) {
+        const { nome } = req.body;
+
+        try {
+            const newTorra = await Torra.create({
+                nome
+            })
+            res.status(200).json(newTorra)
+        } catch (err) {
+            res.status(400).json(err)
+        }
+    },
+    async listarTorra(req, res){
+        try {
+            const lista_torra = await Torra.findAll()
+            res.status(200).json(lista_torra)
+        } catch (err) {
+            res.status(400).json(err)
+        }
+    },
+    async buscarUmTorra(req, res){
+        try {
+            const item = await Torra.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            if (item !== null) {
+                res.status(200).json(item)
+            } else {
+                res.status(402).json({ "Mensagem": "Nada foi encontrado" })
+            }
+        } catch (err) {
+            res.status(400).json(err)
+        }
+    },
+    async deletarTorra(req, res) {
+        try{
+            const item = await Torra.destroy({
+                where: {
+                    id:req.params.id
+                }
+            })
+            res.status(201).json({"Mensagem":"Deletado com sucesso"})
+        }catch (err) {
+            res.status(401).json({ "Mensagem": "Erro ao tentar deletar região!" })
+        }
+    },
+    async atualizarTorra(req, res) {
+        const {nome, descricao} = req.body
+        try{
+            const item = await Torra.update({
+                nome,
+                descricao
+            },
+                {
+                    where: {
+                        id: req.params.id,
+                    }
+                }
+            )
+            if(item !== null){
+                res.status(201).json({ "Mensagem": "Atualizada como sucesso!" })
+            }else{
+                res.status(401).json({ "Mensagem": "Não exite esse id!" })
+            }
+
+        }catch(err){
+            res.status(401).json({ "Mensagem": "Erro ao Atualizada!" })
+        }
+    }
 }
 module.exports = controllerTorra

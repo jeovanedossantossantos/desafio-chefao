a36780f641c1fe1c84e6e7bcd14983bd7adf95bf
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-04 20:00:59 -0300

    Ajustes de carousel

--- front-cafe/src/components/Slider/index.tsx
+++ front-cafe/src/components/Slider/index.tsx
@@ -40,105 +40,69 @@ export const Slider: React.FC = () => {
     <SliderContent>
 
 
-      {/* <Carousel>
-        <Carousel.Item> */}
-          <Row style={{ justifyContent: "center",alignItems: "center",width: "100%", maxWidth: 1500, margin: "auto" }}>
-            <Col className="sumir" xs={3} style={{ margin: 10, justifyContent: "right" }}>
-              <h4>NOTAS</h4>
-              <h3>{vetor?.nota}</h3>
-              <h4>ALTITUDE</h4>
-              <h3>{vetor?.regiao.altitude}</h3>
-              <h4>TORRA</h4>
-              <h3>{vetor?.torra.nome}</h3>
-
-            </Col>
-            <Col className="w-400" xs={5} style={{ margin: 10 }}>
-              <Carousel>
-                <Carousel.Item>
-                  <img style={{ minHeight: "200px", maxHeight: "400px" }}
-                    className="d-block w-100 h-100"
-                    src={vetor?.foto_cafe ? vetor?.foto_cafe : "https://alavoura.com.br/wp-content/uploads/2020/06/tim-e1592429928966.jpeg"}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ minHeight: "200px", maxHeight: "400px" }}
-                    className="d-block w-100 h-100"
-                    src={vetor?.foto_cafe_1 ? vetor?.foto_cafe_1 : "https://alavoura.com.br/wp-content/uploads/2020/06/article.png"}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    style={{ minHeight: "200px", maxHeight: "400px" }}
-                    className="d-block w-100 h-100"
-                    src={vetor?.foto_cafe_2 ? vetor.foto_cafe_2 : "https://alavoura.com.br/wp-content/uploads/2020/06/article.png"}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-
-
-            </Col>
-            <Col className="sumir" xs={3} style={{ margin: 10 }}>
-              <h2>{vetor?.nome}</h2>
-              <p>{vetor?.descricao}</p>
-              <h4>CORPO</h4>
-              <h3>{vetor?.corpo.nome}</h3>
-              <h4>VARAIEDADE</h4>
-              <h3>{vetor?.variedade.nome}</h3>
-              <h4>ACIDEZ</h4>
-              <h3>{vetor?.acidez.nome}</h3>
-            </Col>
-          </Row>
-        {/* </Carousel.Item>
-      </Carousel> */}
-
-      {/* {
-        
-            <Carousel style={{overflow: 'hidden'}}>
-              <Carousel.Item >
-                <img  style={{ minHeight:"200px", maxHeight:"400px"}}
-                  className="d-block w-100 h-100"
-                  src={vetor?.foto_cafe ?vetor?.foto_cafe :"https://alavoura.com.br/wp-content/uploads/2020/06/tim-e1592429928966.jpeg"}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
+      <Carousel>
+        {
+          data.map(e => {
+            return (
               <Carousel.Item>
-                <img
-                  style={{ minHeight:"200px", maxHeight:"400px"}}
-                  className="d-block w-100 h-100"
-                  src={vetor?.foto_cafe_1 ? vetor?.foto_cafe_1:"https://alavoura.com.br/wp-content/uploads/2020/06/article.png"}
-                  alt="Second slide"
-                />
+                <Row style={{ justifyContent: "center", alignItems: "center", width: "100%", maxWidth: 1500, margin: "auto" }}>
+                  <Col className="sumir" xs={3} style={{ margin: 10, justifyContent: "right" }}>
+                    <h4>NOTAS</h4>
+                    <h3>{e?.nota}</h3>
+                    <h4>ALTITUDE</h4>
+                    <h3>{e?.regiao.altitude}</h3>
+                    <h4>TORRA</h4>
+                    <h3>{e?.torra.nome}</h3>
 
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+                  </Col>
+                  <Col className="w-400" xs={5} style={{ margin: 10 }}>
+                    {/* <Carousel> */}
+                      {/* <Carousel.Item> */}
+                        <img style={{ minHeight: "200px", maxHeight: "400px" }}
+                          className="d-block w-100 h-100"
+                          src={e?.foto_cafe ? e?.foto_cafe : "https://alavoura.com.br/wp-content/uploads/2020/06/tim-e1592429928966.jpeg"}
+                          alt="First slide"
+                        />
+                      {/* </Carousel.Item>
+                      <Carousel.Item>
+                        <img
+                          style={{ minHeight: "200px", maxHeight: "400px" }}
+                          className="d-block w-100 h-100"
+                          src={vetor?.foto_cafe_1 ? vetor?.foto_cafe_1 : "https://alavoura.com.br/wp-content/uploads/2020/06/article.png"}
+                          alt="Second slide"
+                        />
+                      </Carousel.Item> */}
+                      {/* <Carousel.Item>
+                        <img
+                          style={{ minHeight: "200px", maxHeight: "400px" }}
+                          className="d-block w-100 h-100"
+                          src={vetor?.foto_cafe_2 ? vetor.foto_cafe_2 : "https://alavoura.com.br/wp-content/uploads/2020/06/article.png"}
+                          alt="Third slide"
+                        />
+                      </Carousel.Item> */}
+                    {/* </Carousel> */}
+
+
+                  </Col>
+                  <Col className="sumir" xs={3} style={{ margin: 10 }}>
+                    <h2>{e?.nome}</h2>
+                    <p>{e?.descricao}</p>
+                    <h4>CORPO</h4>
+                    <h3>{e?.corpo.nome}</h3>
+                    <h4>VARAIEDADE</h4>
+                    <h3>{e?.variedade.nome}</h3>
+                    <h4>ACIDEZ</h4>
+                    <h3>{e?.acidez.nome}</h3>
+                  </Col>
+                </Row>
               </Carousel.Item>
-              <Carousel.Item>
-                <img
-                 style={{ minHeight:"200px", maxHeight:"400px"}}
-                  className="d-block w-100 h-100"
-                  src={vetor?.foto_cafe_2 ? vetor.foto_cafe_2 :"https://alavoura.com.br/wp-content/uploads/2020/06/article.png"}
-                  alt="Third slide"
-                />
+            )
+          })
+        }
 
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-       
-      } */}
-
+      </Carousel>
     </SliderContent>
+
+
   );
 }

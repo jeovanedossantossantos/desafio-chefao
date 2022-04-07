42deaf1cc05e2e800584ecbdca681b791e5007d7
Knziee <16rhuan16@gmail.com>
2022-04-07 00:11:18 -0300

    fix: codigo da api reformatado

--- api-cafe/src/model/index.js
+++ api-cafe/src/model/index.js
@@ -6,73 +6,31 @@ const Moagem = require('./Moagem')
 const Variedade = require('./Variedade')
 const Torra = require('./Torra')
 const Usuarios = require('./Usuarios')
-Cafe.belongsTo(Regiao,{
+
+Cafe.belongsTo(Regiao, {
     foreignKey: 'regiao_id',
-    // through:Regiao,
-    as:"regiao"
+    as: "regiao"
 })
-
-// Regiao.hasMany(Cafe,{
-//     foreignKey: 'regiao_id',
-//     as:"cafe"
-// })
-
-Cafe.belongsTo(Acidez,{
+Cafe.belongsTo(Acidez, {
     foreignKey: 'acidez_id',
-    // through:Regiao,
-    as:"acidez"
+    as: "acidez"
 })
-
-Cafe.belongsTo(Corpo,{
+Cafe.belongsTo(Corpo, {
     foreignKey: 'corpo_id',
-    // through:Regiao,
-    as:"corpo"
+    as: "corpo"
 })
-
-Cafe.belongsTo(Variedade,{
+Cafe.belongsTo(Variedade, {
     foreignKey: 'variedade_id',
-    // through:Regiao,
-    as:"variedade"
+    as: "variedade"
 })
-
-Cafe.belongsTo(Moagem,{
+Cafe.belongsTo(Moagem, {
     foreignKey: 'moagem_id',
-    // through:Regiao,
-    as:"moagem"
+    as: "moagem"
 })
-
-Cafe.belongsTo(Torra,{
+Cafe.belongsTo(Torra, {
     foreignKey: 'torra_id',
-    // through:Regiao,
-    as:"torra"
+    as: "torra"
 })
-
-
-// Regiao.hasMany(Cafe,{
-//     foreignKey: 'regiao_id',
-//     as:"cafe"
-// })
-
-// Cafe.hasMany(Acidez,{
-//     foreignKey: 'acidez_id',
-//     through:Acidez,
-// })
-
-// Cafe.hasMany(Corpo,{
-//     foreignKey: 'corpo_id',
-//     through:Corpo,
-// })
-
-// Cafe.hasMany(Moagem,{
-//     foreignKey: 'moagem_id',
-//     through:Moagem,
-// })
-
-// Cafe.hasMany(Moagem,{
-//     foreignKey: 'moagem_id',
-//     through:Moagem,
-// })
-
 module.exports = {
     Regiao,
     Cafe,
